c52fb332603b94af28588295e2bded51b05d8e4a
양승욱 <yangsw@brandi.co.kr>
2021-09-11 22:49:56 +0900

    setting

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
+import Antd from 'ant-design-vue';
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import 'ant-design-vue/dist/antd.css';
+// import './index.css'
 
-createApp(App).mount('#app')
+// 이 자습서에서는 전역 가져 오기 구성 요소 라이브러리를 사용합니다.
+
+createApp(App).use(router).use(store).use(Antd).mount('#app')
